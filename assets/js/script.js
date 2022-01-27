df991425bf504884fd34ff8bb055fd071ff62285
Mets0811 <melissa0811@icloud.com>
2022-01-26 21:19:59 -0500

    add event selecters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+//Element selecters
+var zipCode=$(".zip-code");
+var jobsBtn=$(".jobs-btn");
+var previousPage=$("#previous-page");
+var nextPage=$("#next-page");
 
+var fetchJobs = (event) => {
+    event.preventDefault();
+    var enteredZip = zipCode.val();
+   
+}
+
+
+//eventlisteners
+jobsBtn.on("click",fetchJobs)
