f2c9fc9fde361c424516094bbb504870147df18c
Mets0811 <melissa0811@icloud.com>
2022-01-26 21:53:14 -0500

    added logic for city capitalzation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,9 @@ var nextPage=$("#next-page");
 
 //global variables
 var currentZip = "";
+var currentCity = "";
+var currentState ="";
+
 
 var jobsHandler = (event) => {
     event.preventDefault();
@@ -14,17 +17,25 @@ var jobsHandler = (event) => {
    
 }
 var fetchLocation= () => {
-    var zipApi= "http://ziptasticapi.com/" + currentZip;
-    fetch(zipApi).then(function(response){
-        if(response.ok){
+    var zipAPI = "http://ziptasticapi.com/" + currentZip;
+    fetch(zipAPI).then(function(response){
             response.json().then(function(data){
                 console.log(data)
+                var cityArr = data.city.split(" ");
+                for (var i = 0; i < cityArr.length; i++) {
+                    cityArr[i] = cityArr[i].charAt(0) + cityArr[i].toLowerCase().slice(1);
+                }
+                currentCity = cityArr.join(" ");
+                currentState = data.state;
+                fetchJobs();
             })
-        }
+    }).catch(function(error){
+        console.log(error)
     })
 }
 
-
+var fetchJobs = () => {
+}
 
 //eventlisteners
 jobsBtn.on("click",jobsHandler)
