e92eaa098b1c31ec2957e67c69df1a99b06241b2
Atticus-Robinson <acerobinson2@gmail.com>
2022-01-26 20:13:21 -0600

    added function for dynamic creation of job cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
+const testArray = ['1', '2', '3', '4', '5'];
 
+function createJobCards(jobsArray) {
+    jobsArray.forEach(function(job, index) {
+        console.log(job, index);
+
+        var jobItem = $("<div>")
+        .addClass("card job-card")
+        .attr("id", index);
+
+        var cardContent = $("<div>")
+        .addClass("card-content white-text");
+
+        var cardTitle = $("<span>")
+        .addClass("card-title")
+        .text("Card Title");
+
+        var cardText = $("<p>")
+        .text(job);
+
+        var cardAction = $("<div>")
+        .addClass("card-action");
+
+        var link = $("<a>")
+        .attr("href", "#")
+        .text("This is a link");
+
+        cardContent.append(cardTitle, cardText);
+        cardAction.append(link);
+        jobItem.append(cardContent, cardAction);
+        $(".cards-container").append(jobItem);
+
+    })
+}
+
+createJobCards(testArray);
